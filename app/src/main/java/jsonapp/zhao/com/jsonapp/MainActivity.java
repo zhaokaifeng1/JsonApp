18e4181249d8be493cbaba93be03778ec01430aa
zhaokaifeng1 <1739862519@qq.com>
2018-03-20 23:10:28 +0800

    Initial commit

---
+++ app/src/main/java/jsonapp/zhao/com/jsonapp/MainActivity.java
@@ -0,0 +1,13 @@
+package jsonapp.zhao.com.jsonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
